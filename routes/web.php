a24bc3c287b7715b1e56146995721df2a11894b7
jpipe420 <jpcano360@gmail.com>
2025-07-09 00:13:20 -0500

    Primer commit: inicialización del proyecto

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('template');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/401', function () {
+    return view('pages.401');
+});
+
+Route::get('/404', function () {
+    return view('pages.404');
+});
+
+Route::get('/500', function () {
+    return view('pages.500');
+});
+
+Route::get('/blank', function () {
+    return view('pages.blank');
+});
